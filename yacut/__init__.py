db3beae329e4eff67606c7fff7484d00ecc23281
Aleksandr Loshchilov <loshchilov.aleksandr@gmail.com>
2023-01-13 15:38:03 +0300

    Fixed autotest on server side. I hope ...

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
-from flask_restful import Api
-from flask_restful_swagger import swagger
 from flask_sqlalchemy import SQLAlchemy
 from settings import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-api = swagger.docs(Api(app), apiVersion='1', api_spec_url="/api/v1/spec")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
